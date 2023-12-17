067b44a158000285e925c398c121b3fef3fda7a9
fadiiyah <153964741+fadiiyah@users.noreply.github.com>
2023-12-17 17:42:22 +0700

    Delete resources directory

--- resources/views/errors/503.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('errors::layouts')
-@section('title', __('503'))
-@section('content')
-  <div class="text-center">
-    <div class="error mx-auto" data-text="503">503</div>
-    <p class="lead text-gray-800 mb-4">Page Service Unavailable</p>
-    <a href="{{ url('/') }}">&larr; Back to Home</a>
-  </div>
-@endsection
